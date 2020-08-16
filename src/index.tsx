b86704b02d1fdab87dae54734821a2d43e16eb45
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-08-16 12:49:14 +0500

    Removed React's default sw

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render(
@@ -9,5 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// serviceWorker.register();
