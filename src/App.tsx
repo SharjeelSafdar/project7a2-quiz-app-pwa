9eea40ae3d6f43de0d944351a9ebda7e6e62ef12
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-08-16 15:53:16 +0500

    Lazy loaded StartQuiz, QuestionCard and Results components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, lazy } from 'react';
 import { useAsyncResource, AsyncResourceContent } from 'use-async-resource';
 // Components
-import { SelectQuiz, StartQuiz, QuestionCard, Results, Loading, Error } from './components/components';
+import { SelectQuiz, /* StartQuiz, QuestionCard, Results, */ Loading, Error } from './components/components';
 // Context
 import { GlobalProvider } from './context/context';
 // Api function
 import { fetchQuestions, dataModifier } from './API/API';
 // Styles
 import { GlobalStyle, Title, Box } from './styles/styles';
+
+const StartQuiz = lazy( () => import('./components/StartQuiz/StartQuiz') );
+const QuestionCard = lazy( () => import('./components/QuestionCard/QuestionCard') );
+const Results = lazy( () => import('./components/Results/Results') );
 
 const App = () => {
   const [ quizReader, fetchNewQuiz ] = useAsyncResource(fetchQuestions)
